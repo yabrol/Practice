af9dc4024d57de91cbae33bf974bdd68cdbfaf6a
Yukti Abrol <yukti.abrol@gmail.com>
2014-10-22 03:05:17 -0400

    allow button to click and change text

--- FunFacts/app/src/main/java/com/yukti/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/yukti/funfacts/FunFactsActivity.java
@@ -1,9 +1,15 @@
 package com.yukti.funfacts;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,6 +18,23 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //initialize view vars
+        final TextView factLabel = (TextView)findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //button clicked, update with new fact
+                String fact = "";
+                //random select facts
+                Random random = new Random();
+                //update with new fact
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
 
