df9a70203022288e520779f761fea62d9d823a7c
Yukti Abrol <yukti.abrol@gmail.com>
2014-10-22 03:31:21 -0400

    changed if/else to string array

--- FunFacts/app/src/main/java/com/yukti/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/yukti/funfacts/FunFactsActivity.java
@@ -27,20 +27,22 @@ public class FunFactsActivity extends Activity {
             @Override
             public void onClick(View view) {
                 //button clicked, update with new fact
-                String fact = "";
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
                 //random select facts
                 Random random = new Random();
-                int num = random.nextInt(3);
+                int num = random.nextInt(facts.length);
                 //update with new fact
-                if(num == 0){
-                    fact="Ants stretch when they wake up in the morning";
-                }
-                else if(num == 1){
-                    fact="Ostriches run faster than horses";
-                }
-                else if (num == 2){
-                    fact = "Olympic gold medals are actually made mostly of silver";
-                }
+                String fact = facts[num];
                 factLabel.setText(fact);
             }
         };
